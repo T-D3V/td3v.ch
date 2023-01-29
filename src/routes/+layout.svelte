d5f8aae89925d0f9357a472ebcbe69e283a6a398
td3v <timo.angst@bluewin.ch>
2023-01-29 16:09:32 +0000

    add start page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,15 @@
 <script>
   import "../styles/app.css"
 </script>
-<slot />
+<header class="bg-transparent fixed flex justify-between w-full px-6 py-4 h-20">
+  <a sapper:prefetch href="/"><img src="/logo_site.png" alt="Cyber Themed Badge" class="h-full hover:scale-95 ease-in-out duration-300"></a>
+  <button id="toggle-nav" class="h-full ease-in-out duration-300 hover:scale-95 hover:text-gray-900"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-full">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
+  </svg></button>
+</header>
+<main>
+  <slot />
+</main>
+<footer class="bg-transparent fixed flex justify-center w-full  px-6 py-4 h-16 bottom-0">
+  <p class="font-robotomono font-extralight text-sky-500">made by &copy;<a href="https://github.com/T-D3V" class="hover:text-sky-600 ease-in-out duration-300" target="_blank" rel="noreferrer">TD3V</a>, {new Date().getFullYear()}</p>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,13 @@
-<div class="h-screen grid place-content-center bg-gray-300">
-  <h1 class="text-5xl text-sky-500 font-roboto font-bold">Welcome to SvelteKit</h1>
-  <p class="text-xl text-red-500 font-robotomono font-extralight">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import TypeWriter from 'svelte-typewriter'
+</script>
+<div class="h-screen bg-gradient-to-b from-sky-900 to-gray-900 pt-20 pb-16 px-4 flex flex-col justify-center items-center">
+  <TypeWriter mode="cascade" keepCursorOnFinish>
+    <div class="flex justify-center flex-col items-center">
+      <img class="h-40 w-40 rounded-full border-sky-500 border-4 shadow-xl" src="/logo_td3v.png" alt="Profile of TD3V" data-static />
+      <h1 class="font-roboto font-bold text-white text-7xl mb-6" data-static>TD3V</h1>
+      <h2 class="font-roboto font-regular text-white text-2xl mb-12">Developer, Voice Actor & Cyber Security  enthusiast.</h2>
+      <a sapper:prefetch href="/about" class="rounded-md bg-transaprent border-sky-500 border-2 text-sky-500 font-robotomono font-regular px-2 py-1 hover:bg-sky-500 ease-in-out duration-300 hover:text-white" data-static>More Info!</a>
+    </div>
+  </TypeWriter>
 </div>
