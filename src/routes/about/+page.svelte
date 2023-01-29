db93aaae888a5ac60441f845e7572e1f038d10c8
td3v <timo.angst@bluewin.ch>
2023-01-29 16:22:47 +0000

    error and about me

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,29 @@
+<div class="h-screen bg-gradient-to-b from-sky-900 to-gray-900 pt-20 pb-16 px-4 flex flex-col items-center">
+  <div class="w-full md:w-[50%]">
+    <h1 class="font-roboto font-bold text-white text-4xl mb-6">HEY IT'S ME TD3V</h1>
+    <img class="h-20 w-20 rounded-full border-sky-500 border-4 shadow-xl mb-6" src="/logo_td3v.png" alt="Profile of TD3V" />
+    <h2 class="font-roboto font-regular text-white text-3xl">SUMMARY:</h2>
+    <p class="font-robotomono font-extralight text-slate-200 mb-4">My name is TD3V, well not my real name but you get it.<br>I'm a 17 year old developer from switzerland and for security purposes I work at a company which won't be named, doing web development.</p>
+    <h2 class="font-roboto font-regular text-white text-3xl">SKILLS:</h2>
+    <ul class="list-disc list-inside text-slate-200 font-robotomono font-extralight mb-4">
+      <li>PHP</li>
+      <li>Laminas</li>
+      <li>Javascript</li>
+      <li>Typescript</li>
+      <li>Svelte</li>
+      <li>Sveltekit</li>
+      <li>Python</li>
+    </ul>
+    <h2 class="font-roboto font-regular text-white text-3xl">INTERESTS:</h2>
+    <ul class="list-disc list-inside text-slate-200 font-robotomono font-extralight mb-4">
+      <li>Web Technologies</li>
+      <li>Cyber Security</li>
+      <li>Voice Acting</li>
+      <li>Chat & Content Moderation</li>
+      <li>Games</li>
+      <li>Movies & Series</li>
+      <li>Music</li>
+      <li>Skiing</li>
+    </ul>
+  </div>
+</div>
