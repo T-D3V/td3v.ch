2f091236879cae156e80c247fd7786574d40eecf
td3v <timo.angst@bluewin.ch>
2023-01-29 18:43:35 +0000

    add projects page and email

---
+++ src/components/card.svelte
@@ -0,0 +1,29 @@
+<script>
+  /**
+  * @type {string}
+  */
+  export let imgUrl
+  /**
+  * @type {string}
+  */
+  export let refLink
+  /**
+  * @type {string}
+  */
+  export let title
+  /**
+  * @type {string}
+  */
+  export let alt
+</script>
+
+<div class="rounded-lg shadow-lg bg-black max-w-sm">
+  <a href="{refLink}" target="_blank" rel="noreferrer">
+    <img class="rounded-t-lg" src="{imgUrl}" alt="{alt}"/>
+  </a>
+  <div class="p-6">
+    <p class="text-white text-xl font-robotomono font-normal mb-3">{title}</p>
+    <slot/>
+    <a href="{refLink}" target="_blank" rel="noreferrer" class="rounded-md bg-transaprent border-sky-500 border-2 text-sky-500 font-robotomono font-normal px-2 py-1 hover:bg-sky-500 ease-in-out duration-300 hover:text-white">Goto Project</a>
+  </div>
+</div>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,7 +4,14 @@
 <div class="h-screen bg-gradient-to-b from-sky-900 to-gray-900 pt-20 pb-16 px-4 flex flex-col items-center">
   <div class="w-full md:w-[50%]">
     <h1 class="font-roboto font-bold text-white text-4xl mb-6">HEY IT'S ME TD3V</h1>
-    <img class="h-20 w-20 rounded-full border-sky-500 border-4 shadow-xl mb-6" src="/logo_td3v.png" alt="Profile of TD3V" />
+    <img class="h-20 w-20 rounded-full border-sky-500 border-4 shadow-xl mb-2" src="/logo_td3v.png" alt="Profile of TD3V" />
+    <p class="mb-6">
+      <a href="mailto:td3v@voicesofwynn.com" class="text-slate-200 font-robotomono font-normal hover:text-slate-400 ease-in-out duration-300"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 inline">
+        <path d="M3 4a2 2 0 00-2 2v1.161l8.441 4.221a1.25 1.25 0 001.118 0L19 7.162V6a2 2 0 00-2-2H3z" />
+        <path d="M19 8.839l-7.77 3.885a2.75 2.75 0 01-2.46 0L1 8.839V14a2 2 0 002 2h14a2 2 0 002-2V8.839z" />
+      </svg> td3v@vociesofwynn.com
+      </a>
+    </p>
     <h2 class="font-roboto font-normal text-white text-3xl">SUMMARY:</h2>
     <p class="font-robotomono font-extralight text-slate-200 mb-4">My name is TD3V, well not my real name but you get it.<br>I'm a 17 year old developer from switzerland and for security purposes I work at a company which won't be named, doing web development.</p>
     <h2 class="font-roboto font-normal text-white text-3xl">SKILLS:</h2>
