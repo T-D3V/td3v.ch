b2432340acf8eec0ec288f690ac69f81fba77406
td3v <timo.angst@bluewin.ch>
2023-01-29 12:03:48 +0000

    app: init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<div class="h-screen grid place-content-center bg-gray-300">
+  <h1 class="text-5xl text-sky-500 font-roboto font-bold">Welcome to SvelteKit</h1>
+  <p class="text-xl text-red-500 font-robotomono font-extralight">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
