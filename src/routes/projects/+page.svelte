fa281675192ca0d9f39add871a705262d22992d4
TD3V <td3v+git@td3v.ch>
2023-11-28 09:59:01 +0100

    content: update projects

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -6,12 +6,12 @@
 </svelte:head>
 <div class="h-full min-h-screen bg-gradient-to-b from-sky-900 to-gray-900 pt-20 pb-16 px-4 flex flex-col justify-start items-center">
   <div class="grid grid-cols-1 lg:grid-cols-3 gap-3 w-full md:w-[75%]">
-    <CardPircture refLink="https://github.com/T-D3V/td3v.ch" imgUrl="/projects/td3v_ch.png" title="TD3V Site" alt="Screenshot of td3v.ch website">
+    <CardPircture refLink="https://github.com/T-D3V/td3v.ch" imgUrl="/projects/td3v.ch.png" title="TD3V Site" alt="Screenshot of td3v.ch website">
       <p class="text-slate-200 font-robotomono font-extralight mb-6">
         This is my personal site and links to the git repo
       </p>
     </CardPircture>
-    <CardPircture refLink="https://voicesofwynn.com" imgUrl="/projects/vow.png" title="VoicesOfWynn" alt="Screenshot of Voices of Wynn">
+    <CardPircture refLink="https://voicesofwynn.com" imgUrl="/projects/voicesofwynn.com.png" title="VoicesOfWynn" alt="Screenshot of Voices of Wynn">
       <p class="text-slate-200 font-robotomono font-extralight mb-6">
         Here I'm a web developer, voice actor, voice manager & discord moderator
       </p>
