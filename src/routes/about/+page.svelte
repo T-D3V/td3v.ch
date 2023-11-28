21a1e94bff39bf3ae23d1aa2129a14eddf2e7f57
TD3V <td3v+git@td3v.ch>
2023-11-28 09:52:38 +0100

    content: change personal information

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,14 +6,14 @@
     <h1 class="font-roboto font-bold text-white text-4xl mb-6">HEY IT'S ME TD3V</h1>
     <img class="h-20 w-20 rounded-full border-sky-500 border-4 shadow-xl mb-2" src="/logo_td3v.png" alt="Profile of TD3V" />
     <p class="mb-6">
-      <a href="mailto:td3v@voicesofwynn.com" class="text-slate-200 font-robotomono font-normal hover:text-slate-400 ease-in-out duration-300"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 inline">
+      <a href="mailto:td3v@td3v.ch" class="text-slate-200 font-robotomono font-normal hover:text-slate-400 ease-in-out duration-300"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 inline">
         <path d="M3 4a2 2 0 00-2 2v1.161l8.441 4.221a1.25 1.25 0 001.118 0L19 7.162V6a2 2 0 00-2-2H3z" />
         <path d="M19 8.839l-7.77 3.885a2.75 2.75 0 01-2.46 0L1 8.839V14a2 2 0 002 2h14a2 2 0 002-2V8.839z" />
-      </svg> td3v@voicesofwynn.com
+      </svg> td3v@td3v.ch
       </a>
     </p>
     <h2 class="font-roboto font-normal text-white text-3xl">SUMMARY:</h2>
-    <p class="font-robotomono font-extralight text-slate-200 mb-4">My name is TD3V, well not my real name but you get it.<br>I'm a 17 year old developer from switzerland and for security purposes I work at a company which won't be named, doing web development.</p>
+    <p class="font-robotomono font-extralight text-slate-200 mb-4">My name is TD3V, well not my real name but you get it.<br>I'm an 18 year old developer from switzerland and for security purposes I work at a company which won't be named, doing web development.</p>
     <h2 class="font-roboto font-normal text-white text-3xl">SKILLS:</h2>
     <ul class="list-disc list-inside text-slate-200 font-robotomono font-extralight mb-4">
       <li>PHP</li>
@@ -28,9 +28,8 @@
     <ul class="list-disc list-inside text-slate-200 font-robotomono font-extralight mb-4">
       <li>Web Technologies</li>
       <li>Cyber Security</li>
-      <li>Voice Acting</li>
+      <li>CTFs</li>
       <li>Chat & Content Moderation</li>
-      <li>Games</li>
       <li>Movies & Series</li>
       <li>Music</li>
       <li>Skiing</li>
